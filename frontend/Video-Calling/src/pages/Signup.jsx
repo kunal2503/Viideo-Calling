4c8ff08d79e0dc5dc5c7c8ea6ad0f9149ac447dc
kunal2503 <kunaldeshmukh07d@email.com>
2025-08-29 14:57:07 +0530

    inicial commit

---
+++ frontend/Video-Calling/src/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Signup = () =>{
+    const handleSignup = async() =>{
+
+    }
+    return(
+        <div className="">
+            <div className="">
+
+            </div>
+        </div>
+    );
+}
+
+export default  Signup;
